df057549a0f709a4d13dddb09c6a17701aaaa261
luthfipratamaps <luthfipratamaps@gmail.com>
2023-06-19 15:59:28 +0700

    added api key

--- src/helper/Api.js
+++ src/helper/Api.js
@@ -1,26 +1,27 @@
 import axios from 'axios';
 
-const baseApi = 'http://103.156.114.74:40424';
+const baseApi = 'http://localhost:5000';
+const apiKey = process.env.REACT_APP_API_KEY; // Get the API key from the environment variable
 
 class Api {
   static getAvailableDates() {
-    return axios.get(`${baseApi}/available-dates`);
+    return axios.get(`${baseApi}/available-dates`, { headers: { 'x-api-key': apiKey } });
   }
 
   static getDataByDate(date) {
-    return axios.get(`${baseApi}/data/${date}`);
+    return axios.get(`${baseApi}/data/${date}`, { headers: { 'x-api-key': apiKey } });
   }
 
   static getDataByMonth(month) {
-    return axios.get(`${baseApi}/monthly-data/${month}`);
+    return axios.get(`${baseApi}/monthly-data/${month}`, { headers: { 'x-api-key': apiKey } });
   }
   
   static downloadData(date) {
-    return axios.get(`${baseApi}/download-data/${date}`);
+    return axios.get(`${baseApi}/download-data/${date}`, { headers: { 'x-api-key': apiKey } });
   }
 
   static getNodes() {
-    return axios.get(`${baseApi}/nodes`);
+    return axios.get(`${baseApi}/nodes`, { headers: { 'x-api-key': apiKey } });
   }
 }
 
